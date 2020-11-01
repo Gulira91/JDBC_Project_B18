be67cbbe55f20775dc69126a81d90851cae4cc9f
Gulira91 <d.gulira@gmail.com>
2020-11-01 12:43:57 -0500

    My first commit

---
+++ src/test/java/GuliraLearningZFork/java/MyFile.java
@@ -0,0 +1,9 @@
+package GuliraLearningZFork.java;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class MyFile {
+    public static void main(String[] args) {
+        System.out.println("Aaaaaa");
+    }
+}
